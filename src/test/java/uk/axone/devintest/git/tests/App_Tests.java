cea646aae411c09a8a73035c75e7dae84b2b6bb2
praveena <cheerful.praveena@gmail.com>
2023-02-09 20:07:49 +0000

    JIRA - committing for the first time

---
+++ src/test/java/uk/axone/devintest/git/tests/App_Tests.java
@@ -0,0 +1,10 @@
+package uk.axone.devintest.git.tests;
+
+import org.testng.annotations.Test;
+
+public class App_Tests{
+    @Test
+    public void test1(){
+        System.out.println("running test1");
+    }
+}
